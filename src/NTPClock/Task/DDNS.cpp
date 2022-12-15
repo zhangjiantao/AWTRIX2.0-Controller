21540a173660a1f0160373796397fcdae658f3a9
张建涛 <jiantaozhang.zjt@alipay.com>
2022-12-15 15:02:03 +0800

    no message

--- src/NTPClock/Task/DDNS.cpp
+++ src/NTPClock/Task/DDNS.cpp
@@ -12,7 +12,7 @@
 #define HOSTNAME "llvm.f3322.net"
 #define AUTH_NAME "Authorization"
 #define AUTH_VALUE "Basic cm9vdDp6aGFuZ2ppYW50YW8="
-#define CURRENT_API "http://ip.3322.net/"
+#define IP_API "http://ip.3322.net/"
 #define UPDATE_API                                                             \
   "http://members.3322.net/dyndns/update?hostname=" HOSTNAME "&myip="
 
@@ -47,21 +47,26 @@ class DDNS : public Task {
   HttpUtils http;
 
   unsigned long last_update_time = 0;
-  String last{"unknow"};
 
   bool update() {
     int errCode = 0;
-    auto curr = http.httpRequest(CURRENT_API, errCode);
-    curr.trim();
+    auto ip = http.httpRequest(IP_API, errCode);
+    ip.trim();
     if (errCode) {
       LOG(Serial.println("can not get current ip"));
       return false;
     }
 
-    if (curr != last) {
-      LOG(Serial.printf("update from %s to %s\n", last.c_str(), curr.c_str()));
-      auto api = String(UPDATE_API) + curr;
-      auto res = http.httpRequest(api, errCode, true);
+    IPAddress resolve_ip;
+    if (!WiFi.hostByName(HOSTNAME, resolve_ip, 1000)) {
+      LOG(Serial.println("resolve hostname failed"));
+      return false;
+    }
+
+    auto host_ip = resolve_ip.toString();
+    if (ip != host_ip) {
+      LOG(Serial.printf("update from %s to %s\n", host_ip.c_str(), ip.c_str()));
+      auto res = http.httpRequest(String(UPDATE_API) + ip, errCode, true);
       res.trim();
       if (errCode != 0 ||
           (!res.startsWith("good") && !res.startsWith("nochg"))) {
@@ -69,8 +74,6 @@ class DDNS : public Task {
         return false;
       }
       LOG(Serial.println("done"));
-      last = curr;
-      return true;
     }
     return true;
   }
@@ -78,7 +81,7 @@ class DDNS : public Task {
 public:
   bool run() override {
     auto ts = ntp.getEpochTime();
-    if (ts - last_update_time < 5*60)
+    if (ts - last_update_time < 3 * 60)
       return true;
     last_update_time = ts;
     return update();
